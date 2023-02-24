685f8fb55aee0e754870948373c223d402ef997b
devsamuelv <realalloutmusic@gmail.com>
2023-02-24 00:33:56 -0500

    Cleaned up some comments

--- src/main/java/frc/robot/commands/arm/turret/RotateToDegree.java
+++ src/main/java/frc/robot/commands/arm/turret/RotateToDegree.java
@@ -5,15 +5,15 @@
 package frc.robot.commands.arm.turret;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.TurretSubsystem;
 
+/**
+ * Rotate command for the turret. Right now its configured with a dead reckon
+ * controller however it is planned to switch over to pid.
+ */
 public class RotateToDegree extends CommandBase {
   private TurretSubsystem turretSubsystem;
   private boolean isFinished;
@@ -55,33 +55,14 @@ public class RotateToDegree extends CommandBase {
     double achieveableAngle = MathUtil.clamp(desiredAngle, -180, 180);
     double angleDifference = achieveableAngle - currentAngle;
     double powerSign = Math.signum(angleDifference);
-    // double vel = (Math.abs(currentAngle) - Math.abs(st)) / epoch.get();
-
-    // TrapezoidProfile.State currentState = new State(currentAngle, vel);
-    // TrapezoidProfile.State goalState = new State(desiredAngle, 0);
-
-    // TrapezoidProfile profile = new TrapezoidProfile(new Constraints(30, 15),
-    // goalState, currentState);
-    // TrapezoidProfile.State t = profile.calculate(timer.get());
-    // double power = t.velocity * (1 / 36.6);
-
-    // if (profile.isFinished(timer.get())) {
-    // isFinished = true;
-    // }
-
-    // SmartDashboard.putNumber("profile pos", t.position);
-    // SmartDashboard.putNumber("profile vel", t.velocity);
-    // this.turretSubsystem.powerTurretUnsafe(power);
 
     if (Math.abs(angleDifference) < 1) {
       isFinished = true;
     }
     this.turretSubsystem.powerTurret(powerSign);
 
-    // SmartDashboard.putNumber("vel", vel);
     SmartDashboard.putNumber("currentAngle", currentAngle);
     SmartDashboard.putNumber("angleDiff", angleDifference);
-    // SmartDashboard.putNumber("power", power);
     SmartDashboard.putNumber("achieveableAngle", achieveableAngle);
 
     lastAngle = currentAngle;
