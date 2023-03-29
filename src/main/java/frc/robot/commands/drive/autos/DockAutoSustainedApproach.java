e4a926853ff222396afbbfa29c073887ccc3d746
devsamuelv <realalloutmusic@gmail.com>
2023-03-28 23:29:33 -0400

    Changed the order of the parallel command sequence to fix nav bug

---
+++ src/main/java/frc/robot/commands/drive/autos/DockAutoSustainedApproach.java
@@ -0,0 +1,148 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drive.autos;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Watchdog;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Telemetry;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.arm.ArmSubsystem;
+
+public class DockAutoSustainedApproach extends CommandBase {
+  private SwerveSubsystem swerveSubsystem;
+  private double angleRange;
+  private boolean didReachDock;
+  private boolean isFinished;
+  private double reachedDockTimestamp;
+
+  private final double MAX_POWER_METERS;
+  private final double MAX_ANGLE;
+  private final double APPROACH_SPEED;
+
+  private final Watchdog watchdog;
+
+  /** Creates a new DockAuto. */
+  public DockAutoSustainedApproach(SwerveSubsystem swerveSubsystem, double angleRange, double MAX_POWER_METERS,
+      double MAX_ANGLE,
+      double APPROACH_SPEED) {
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    this.swerveSubsystem = swerveSubsystem;
+    this.angleRange = angleRange;
+    this.didReachDock = false;
+    this.isFinished = false;
+
+    this.watchdog = new Watchdog(15, () -> {
+      this.swerveSubsystem
+          .setModuleStates(Constants.DriveConstants.kDriveKinematics.toSwerveModuleStates(new ChassisSpeeds()));
+
+      this.isFinished = true;
+    });
+
+    this.MAX_POWER_METERS = MAX_POWER_METERS;
+    this.APPROACH_SPEED = APPROACH_SPEED;
+    this.MAX_ANGLE = MAX_ANGLE;
+
+    addRequirements(swerveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.didReachDock = false;
+    this.reachedDockTimestamp = 0;
+    this.isFinished = false;
+    this.watchdog.reset();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double currentPitch = swerveSubsystem.getPitch();
+    double approachPitchThreshold = 12;
+    double pitchSettle = 12; // 12
+    double timerThreshold = .4;
+    // 14 might be too high.
+
+    if (currentPitch < approachPitchThreshold) {
+      reachedDockTimestamp = 0;
+    }
+
+    if (currentPitch >= approachPitchThreshold && !didReachDock) {
+      if (reachedDockTimestamp == 0) {
+        reachedDockTimestamp = Timer.getFPGATimestamp();
+      }
+
+      if ((reachedDockTimestamp - Timer.getFPGATimestamp()) > timerThreshold) {
+        didReachDock = true;
+        watchdog.reset();
+      }
+
+    } else if (!didReachDock) {
+      this.swerveSubsystem.setModuleStates(
+          Constants.DriveConstants.kDriveKinematics.toSwerveModuleStates(new ChassisSpeeds(APPROACH_SPEED, 0, 0)));
+    }
+
+    if (didReachDock) {
+      double gain = 0.059;
+      double power = gain * currentPitch;
+
+      /* Power ceiling and floor */
+      /*
+       * if (Math.abs(currentPitch) > MAX_ANGLE) {
+       * power = Math.copySign((MAX_POWER_METERS / MAX_ANGLE) * MAX_ANGLE,
+       * currentPitch);
+       * } else if (Math.abs(currentPitch) < pitchFloor) {
+       * power = 0;
+       * 
+       * isFinished = true;
+       * }
+       */
+
+      if (Math.abs(currentPitch) < pitchSettle) {
+        // this.stablizeTimer.start();
+        power = 0;
+        isFinished = true;
+      }
+      // else {
+      // stablizeTimer.stop();
+      // stablizeTimer.reset();
+      // }
+
+      // if (stablizeTimer.hasElapsed(.3)) {
+      // }
+
+      swerveSubsystem.setModuleStates(
+          Constants.DriveConstants.kDriveKinematics.toSwerveModuleStates(new ChassisSpeeds(power, 0, 0)));
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    swerveSubsystem
+        .setModuleStates(Constants.DriveConstants.kDriveKinematics.toSwerveModuleStates(new ChassisSpeeds()));
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    boolean didFinish = isFinished;
+    boolean watchdogExpired = watchdog.isExpired();
+
+    if (didFinish) {
+      Telemetry.logData("---- Dock Auto ----; didFinish", didFinish, DockAutoSustainedApproach.class);
+    }
+
+    if (watchdogExpired) {
+      Telemetry.logData("---- Dock Auto ----; watchdog expired", watchdogExpired, DockAutoSustainedApproach.class);
+    }
+
+    return didFinish || watchdogExpired;
+  }
+}
--- src/main/java/frc/robot/commands/drive/autos/ScoreHighCenterAndLeaveCommunity.java
+++ src/main/java/frc/robot/commands/drive/autos/ScoreHighCenterAndLeaveCommunity.java
@@ -5,36 +5,38 @@
 package frc.robot.commands.drive.autos;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.arm.UtilityCommands;
-import frc.robot.commands.arm.extend.ExtendTicksPlus;
-import frc.robot.commands.arm.grabber.GrabberOpen;
 import frc.robot.commands.drive.DriveByGyroInfinity;
 import frc.robot.commands.drive.DriveStraightByGyro;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.arm.ArmSubsystem;
-import frc.robot.subsystems.arm.ExtenderSubsystem;
-import frc.robot.subsystems.arm.GrabberSubsystem;
-import frc.robot.subsystems.arm.TurretSubsystem;
 
 /** Add your docs here. */
 public class ScoreHighCenterAndLeaveCommunity {
+  /*
+   * Be careful of the order in which parallel command groups are executed because
+   * when the command at the end of the group ends all the rest end even if they
+   * are done.
+   */
   public static Command leaveCommunityCenter(SwerveSubsystem swerveSubsystem, ArmSubsystem armSubsystem) {
     return new DriveStraightByGyro(-2, 2.2, false, swerveSubsystem)
         .alongWith(UtilityCommands.pivotArm(200, armSubsystem))
-        .andThen(new DriveByGyroInfinity(-1.8, -8, 1.8, false, swerveSubsystem)
-            .alongWith(UtilityCommands.pivotArm(160, armSubsystem)))
+        .andThen(
+            UtilityCommands.pivotArm(160, armSubsystem).alongWith(new DriveByGyroInfinity(-1.8, -8, 1.8, false,
+                swerveSubsystem)))
         .andThen(new DriveStraightByGyro(-1.6, 2.2, swerveSubsystem));
   }
 
   public static Command leaveCommunityRight(SwerveSubsystem swerveSubsystem, ArmSubsystem armSubsystem) {
-    return new DriveStraightByGyro(-2, 1.8, false, swerveSubsystem)
-        .alongWith(UtilityCommands.pivotArm(200, armSubsystem))
-        .andThen(new DriveByGyroInfinity(-1.8, -8, 1.5, false, swerveSubsystem)
-            .alongWith(UtilityCommands.pivotArm(160, armSubsystem)))
+    return UtilityCommands.pivotArm(200, armSubsystem)
+        .alongWith(new DriveStraightByGyro(-2, 1.8, false, swerveSubsystem))
+        .andThen(new DriveByGyroInfinity(-1.8, -8, 1.5, false, swerveSubsystem).alongWith(UtilityCommands.pivotArm(160,
+            armSubsystem)))
         .andThen(new DriveStraightByGyro(-1.6, 1.8, swerveSubsystem));
   }
 
   public static Command balanceStation(SwerveSubsystem swerveSubsystem) {
-    return new DockAuto(swerveSubsystem, 0, 2, 37, 3.0);
+    return new DockAutoSustainedApproach(swerveSubsystem, 0, 2, 37, 3.0);
   }
 }
